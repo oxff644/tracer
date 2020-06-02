b5f832d371c0ef756a9e8aae756c2beb3d3758b2
0xff <61901987+oxff644@users.noreply.github.com>
2020-06-02 09:24:26 +0800

    Update base.py

--- scripts/base.py
+++ scripts/base.py
@@ -6,5 +6,5 @@ import django
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_dir)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s25.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracer.settings")
 django.setup()  # os.environ['DJANGO_SETTINGS_MODULE']
