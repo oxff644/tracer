40c8dd91ca46d8d096c4e9dd6a8c80d43e10a636
0xff <61901987+oxff644@users.noreply.github.com>
2020-06-02 09:29:21 +0800

    Update wsgi.py

--- tracer/wsgi.py
+++ tracer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s25.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracer.settings")
 
 application = get_wsgi_application()
